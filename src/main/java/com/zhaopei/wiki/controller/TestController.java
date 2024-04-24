8ad60435ada2b6ffa69da815f0fc227795526e34
zhaopei <424481223@qq.com>
2024-04-24 16:13:35 +0800

    2-9 配置热部署

--- src/main/java/com/zhaopei/wiki/controller/TestController.java
+++ src/main/java/com/zhaopei/wiki/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World2";
     }
 }
