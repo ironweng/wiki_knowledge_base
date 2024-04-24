5370322b6b65f7b41e2560349b2dc52dbf6c1a1a
zhaopei <424481223@qq.com>
2024-04-24 14:43:07 +0800

    2-5 修改启动文案

--- src/main/java/com/zhaopei/wiki/WikiApplication.java
+++ src/main/java/com/zhaopei/wiki/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.zhaopei.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @SpringBootApplication
 public class WikiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app=new SpringApplication(WikiApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        log.info("启动成功!");
+        log.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
