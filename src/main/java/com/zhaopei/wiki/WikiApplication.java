5c3863080a6df9f48e755c24617e3f7c04cdf93b
zhaopei <424481223@qq.com>
2024-04-24 15:09:31 +0800

    2-6 开发了一个Hello World接口

--- src/main/java/com/zhaopei/wiki/WikiApplication.java
+++ src/main/java/com/zhaopei/wiki/WikiApplication.java
@@ -10,12 +10,10 @@ import java.util.logging.Logger;
 @Slf4j
 @SpringBootApplication
 public class WikiApplication {
-
     public static void main(String[] args) {
         SpringApplication app=new SpringApplication(WikiApplication.class);
         Environment env=app.run(args).getEnvironment();
         log.info("启动成功!");
         log.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
-
 }
