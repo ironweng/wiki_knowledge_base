b3143fdee21acd46ed3ff256cb8d1362489397bf
zhaopei <424481223@qq.com>
2024-04-26 13:35:22 +0800

    3-5 集成mybatis generator ,生成demo表

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,17 @@ CREATE TABLE example_table (
 );
 
 insert into  test (id, name, password) values (1,'测试','123456');
+
+drop table if exists example_table;
+CREATE TABLE example_table (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               name varchar(50)
+);
+
+insert into  test (id, name, password) values (1,'测试','123456');
+
+drop table if exists demo;
+CREATE TABLE demo (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               name varchar(50)
+);
