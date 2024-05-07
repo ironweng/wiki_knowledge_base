587baeffdd0565726aa49133905a486c409d8db4
zhaopei <424481223@qq.com>
2024-05-07 22:40:30 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/zhaopei/wiki/service/EbookService.java
+++ src/main/java/com/zhaopei/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.zhaopei.wiki.entity.EbookExample;
 import com.zhaopei.wiki.mapper.EbookMapper;
 import com.zhaopei.wiki.req.EbookReq;
 import com.zhaopei.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.zhaopei.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,12 +25,8 @@ public class EbookService {
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         //查询出来的ebookList赋给respList返回给前端
         //(这样做是为了规范,实体类+Req是前端传来的封装查询参数的类,实体类+Resp是后端封装了返回数据的类)
-        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp=new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+
+        List<EbookResp> respList= CopyUtil.copyList(ebookList,EbookResp.class);
         return respList;
     }
 }
