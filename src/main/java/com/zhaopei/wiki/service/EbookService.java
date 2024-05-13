f88f24c20e996548a974dbf126e15b91f4df8e3d
zhaopei <424481223@qq.com>
2024-05-13 14:17:55 +0800

    5-4 列表查询接口支持动态SQL

--- src/main/java/com/zhaopei/wiki/service/EbookService.java
+++ src/main/java/com/zhaopei/wiki/service/EbookService.java
@@ -8,6 +8,7 @@ import com.zhaopei.wiki.resp.EbookResp;
 import com.zhaopei.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -21,7 +22,9 @@ public class EbookService {
         //以下两句是固定写法
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%"+req.getName()+"%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         //查询出来的ebookList赋给respList返回给前端
         //(这样做是为了规范,实体类+Req是前端传来的封装查询参数的类,实体类+Resp是后端封装了返回数据的类)
