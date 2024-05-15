9d47b901abacdc021128daf20d7bdfaa53225fda
zhaopei <424481223@qq.com>
2024-05-15 09:54:01 +0800

    5-9  增加AOP,打印接口耗时，请求参数和返回参数，去掉过滤器和拦截器

--- src/main/java/com/zhaopei/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/zhaopei/wiki/config/SpringMvcConfig.java
@@ -1,33 +1,33 @@
-package com.zhaopei.wiki.config;
-
-import com.zhaopei.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/test/**",
-                        "/redis/**",
-                        "/user/login",
-                        "/category/all",
-                        "/ebook/list",
-                        "/doc/all/**",
-                        "/doc/vote/**",
-                        "/doc/find-content/**",
-                        "/ebook-snapshot/**",
-                        "/ebook/upload/avatar",
-                        "/file/**"
-                );
-    }
-}
+//package com.zhaopei.wiki.config;
+//
+//import com.zhaopei.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/test/**",
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/ebook/list",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**",
+//                        "/ebook/upload/avatar",
+//                        "/file/**"
+//                );
+//    }
+//}
